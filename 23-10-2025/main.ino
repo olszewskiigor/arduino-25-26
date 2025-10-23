e6249b84c15930881f88551b3ca51d4920bccfa2
Igor Olszewski <71021482+olszewskiigor@users.noreply.github.com>
2025-10-23 17:27:28 +0200

    Create main.ino

---
+++ 23-10-2025/main.ino
@@ -0,0 +1,21 @@
+#define TRIG 9
+#define ECHO 10
+
+int ledPin = 13;
+
+void setup()
+{
+	pinMode(ledPin,OUTPUT);
+}
+
+void loop()
+{
+  for (int t = 1000; t > 100; t= t-50){
+     digitalWrite(ledPin,HIGH);
+  	 delay(t);
+  	 digitalWrite(ledPin,LOW);
+     delay(t);
+  }
+  // Robimy dodatkową przerwę
+  delay(4000);
+}
