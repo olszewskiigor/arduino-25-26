4f6e84a99b2d749e8ab6cfaeb9c4d6fd79d720e3
Igor Olszewski <71021482+olszewskiigor@users.noreply.github.com>
2025-11-06 18:16:19 +0100

    Create main.ino

---
+++ 06-11-2025/main.ino
@@ -0,0 +1,16 @@
+// Dodajemy do naszego programu potrzebne nam biblioteki
+#include <SPI.h>
+#include <MFRC522.h> // Biblioteka obsługująca czytnik RFID RC522
+
+// Ustalamy, które piny są podłączone z modułem RFID:
+#define SS_PIN 10 // Pin podłączony z pinem D10 Arduino
+#define RST_PIN 5 // Pin RST podłączony z pinem D5 Arduino
+
+void setup() {
+  Serial.begin(9600); // Uruchamiamy komunikację z komputerem (monitor portu szeregowego)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
